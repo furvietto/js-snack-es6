4c596e2b29fbc91d7e09f17ebf5e1816847c3b7a
furvietto <fulvio-94@hotmail.it>
2021-12-14 17:41:27 +0100

    add fixing , remove ...

--- script.js
+++ script.js
@@ -4,8 +4,21 @@
 // Usiamo i nuovi metodi degli array foreach o filter.
 
 
+//bonus
+// function generateArray(min, max, ...myarguments) {
+//    const include = myarguments.filter((element,index) => {
+//         // return
+//         return index > min && index < max
+        
+//    })
+//    return include
+// }
 
-function generateArray(min, max, ...myarguments) {
+// //log
+// console.log(generateArray(2,6,2,3,4,5,6,7,8,9));
+
+
+function generateArray(min, max, myarguments) {
    const include = myarguments.filter((element,index) => {
         // return
         return index > min && index < max
@@ -15,4 +28,4 @@ function generateArray(min, max, ...myarguments) {
 }
 
 //log
-console.log(generateArray(2,6,2,3,4,5,6,7,8,9));
+console.log(generateArray(2,6,[0,1,2,3,4,5,6,7,8,9]));
